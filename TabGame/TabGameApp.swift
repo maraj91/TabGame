8f09eec09507d6711b94ac72ee56125f44a3798b
maraj hussain <maraj.hussain75@gmail.com>
2025-03-03 12:36:19 +0530

    Initial Commit

---
+++ TabGame/TabGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabGameApp.swift
+//  TabGame
+//
+//  Created by Maraj Hussain on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TabGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
